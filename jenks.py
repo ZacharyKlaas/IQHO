6cc6ee4367134368fa0ce1dc50e9837c65463eac
Zachary Klaas <zachary.klaas@ymail.com>
2016-04-28 17:58:50 -0400

    Update jenks.py
    Final!  I think.

--- jenks.py
+++ jenks.py
@@ -1,87 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##################################################################
+# Écrit par Zachary Klaas
+# Implementation ArcPy de l'analyse Jenks Natural Breaks
+# En utilisant les utiles PySAL (Python Spatial Analysis Library)
+# Version pour l'usage avec ArcMap 10.3 et après
+##################################################################
+
 import scipy, pandas, pysal
 import arcpy
 import numpy as np
 import pandas as pd
 from numpy import array
 from pandas import DataFrame
-print "Importing packages..."
-#The arcpy package, of course, is the Python package for ArcGIS functions.  The
-#  pysal package is an open-source package of geography functions that is not
-#  included in ArcGIS, but can be downloaded free on the internet.  The numpy
-#  package is included in more recent distributions of Python, and contains the
-#  numeric array processing functions necessary to speed up calculations for
-#  large arrays.  The scipy package contains scientific functions that are used
-#  to power the functions provided by pysal.  The pandas package contains data
-#  analysis functions used to power the functions provided by pysal.
-#The numpy and pandas packages are instantiated because we will be using elements
-#  of the array and DataFrame functions within those packages.
+print "Importation des progiciels nécessaires..."
+#Le progiciel ArcPy est le progiciel qui met en oeuvre les fonctions ArcGIS dans
+#  le langage informatique de Python.  Le logithèque libre PySAL fournit les fonctions
+#  d'analyse géographique.  C'est ce logithèque-ci qu'on utilise pour exécuter
+#  l'analyse "natural breaks" (catégories naturelles) de George Jenks.  PySAL n'est pas 
+#  inclus avec ArcGIS 10.3; il faut l'installer avant de exécuter ce script.
+#Les progiciels numpy (fonctions numériques et mathématiques), scipy (fonctions
+#  d'analyse scientifique) et pandas (fonctions d'analyse des données) élargissent
+#  les pouvoirs de Python.  Ils sont inclus avec le logiciel ArcGIS 10.3 et après.
+#  Avant de cette version, il faudrait les installer.
+#Les progiciels numpy et pandas sont instanciés comme "np" et "pd" parce qu'on
+#  a besoin des elements du tableau numérique de numpy (Numpy Array) et cadre de
+#  données de pandas(Pandas DataFrame).
 
-print "Importing functions..."
+print "Importation des fonctions PySAL..."
 from pysal.esda.mapclassify import Natural_Breaks as nb
-#The Jenks Natural Breaks function is contained in the pysal package under the
-#  name pysal.esda.mapclassify.Natural_Breaks.  For simplicity's sake we will
-#  instantiate this as nb for later use in the code.
+#La fonction Jenks est partie du progiciel pysal.esda.mapclassify.Natural_Breaks.
+#   Pour simplifier, on va instancier ça comme "nb" a partir de maintenant dans
+#   le script.
 
-print "Converting IQHO to Numpy Array..."
-fc = "f:\\test\\IQH_Temp_join3.shp"
-field = "IQH_Test"
-myArray = arcpy.da.FeatureClassToNumPyArray(fc, field, skip_nulls=True)
+print "Conversion IQHO en version de tableau numérique Numpy (Numpy Array)..."
+fc = arcpy.GetParameterAsText(0)
+field = "IQH_FINAL"
+myArray = arcpy.da.FeatureClassToNumPyArray(fc, field)
 myArray.dtype = np.float32
-#The preceding calculation relies upon the arcpy.da (Data Analysis) and numpy packages.
-#The fc parameter is the feature class upon which the operation is to be performed.
-#The field parameter is the name of the field which is to be categorised into the
-#  Jenks Natural Breaks categories.
-#The "skip_nulls" flag allows null values to be rejected as a basis for the calculation
-#  of the results, when equivalent to "True".  This can be edited out of the script
-#  should it be desired to retain null values, but it should be remembered that null
-#  values are distinct from values of zero, which generally one will want to retain
-#  in calculations.  Null values typically represent "no data" for an area, and as
-#  such, should be excluded from consideration by Jenks Natural Breaks calculations.
-#Though the array is of integers, it is cast as a float because the PYSAL package
-#  element KMEANS requires a float variable.  The results returning from PYSAL
-#  are fed into integer fields, however.
+cursor = arcpy.da.UpdateCursor(fc,field)
+thecount = 0
+for row in cursor:
+	if row[0] < 0:
+		myArray[thecount] = None
+		print thecount
+		thecount += 1
+	else:
+		myArray[thecount] = row[0]
+		thecount += 1
+myDataFrame = pd.DataFrame({"TheData": myArray})
+#La fonction "GetParameterAsText" invite l'utilisateur de nommer le fichier géographique
+#  ("feature class" ou "fc") sur lequel les opérations vont commencer.
+#Ce script utilise "IQH_FINAL" comme le champ des données sur lequel les opérations vont
+#   commencer ("field").
+#La calculation utilise les progiciels arcpy.da (analyse des données) et numpy.
+#Nonobstant que le tableau numérique consiste en nombres entiers, le tableau est
+#   transformé au format de point flottant ("float") parce que le progiciel PySAL
+#   a besoin de cette transformation pour l'intégrer avec la fonction KMEANS.
+#L'iteration "for-if-else" trie les valeurs "null" des vraies données, et après cette
+#   sortation, les données sont transferées en format de cadre des données pandas.
 
-print "Calculating breaks..."
-breaks = nb(myArray.ravel(),k=4,initial=20)
-#The preceding calculation relies upon the pysal package.
-#The Natural_Breaks function only accepts a 1d vector of values; using numpy.ravel()
-#  for this vector.
-#The k parameter is the number of classes to be used by the Jenks function.
-#The initial parameter is the "seed", which can be varied to speed up the calculation.
-#(The tradeoff is speed versus consistency of calculation - since the Jenks function
-#  is calculated by an iterative process, a fast calculation will not converge on the
-#  best "answer" right away, though it will start to do so; a slow calculation, by
-#  contrast, will be more likely to have converged on the best values, though at the
-#  cost of slowness.  This is an important consideration if you're running this on
-#  a big dataset.)
+print "Calcul des Jenks natural breaks..."
+breaks = nb(myDataFrame["TheData"].dropna().values,k=4,initial=20)
+#Le calcul des valeurs Jenks est produit par le progiciel pysal.  Tous les valeurs
+#   "null" sont sortis, et les données qui restent sont préparées pour l'analyse.
+#La paramètre k symbolise le nombre des classes la fonction Jenks va créer pour
+#   l'utilisateur.
+#La paramètre initial est le semence de la fonction Jenks.  Un valeur grand va
+#   converger la fonction plus vite; un valeur petit, d'autre part, va être plus exact. 
 
-print "Checking for any previous calculations of the Jenks field..."
-newfield = "Jenks"
+print "Vérification s'il y avait calculs précédents des champs de valeurs Jenks..."
 try:
-	arcpy.DeleteField_management(fc, newfield)
-	print "Previous calculation of the Jenks field deleted..."
+	arcpy.DeleteField_management(fc, "Jenks")
+	print "Calculs précédents des champs de valeurs Jenks effacés..."
 
 except Exception as e:
-	print "No previous calculation of the Jenks field found..."
-#If there was any field storing previous calculations for the Jenks Natural Breaks values,
-#  this field is deleted, and then the next bit of code after this try-except block will
-#  create this field anew.  If there is no such field, then the try block will throw an
-#  exception rather than the program halting because no such field was found, and the
-#  calculation of a Jenks Natural Breaks field can take place from scratch in the next
-#  block of code.
+	print "Aucuns champs des valeurs Jenks trouvés..."
+#Cette iteration "try-except" efface les calculs précédents s'ils existent. Si un champ
+#   des résultats "Jenks" existe, le partie "try" va l'effacer.  Si un tel champ n'existe
+#   pas, le partie "except" annonce que c'est le cas.
 
-print "Exporting results to feature class table..."
+print "Exportation des resultats au fichier géographique (shapefile)..."
 arcpy.AddField_management(fc,"index","long")
-arcpy.AddField_management(fc,newfield,"long")
-myData = pd.DataFrame(breaks.yb)
-myData.columns = [newfield]
-myReturnArray = myData.to_records()
+arcpy.AddField_management(fc,"Jenks","long")
+myData = pd.DataFrame({"Jenks": breaks.yb}, index=myDataFrame["TheData"].dropna().index)
+joinData = myDataFrame.join(myData)
+joinData["Jenks"].fillna(0, inplace=True)
+joinData.drop("TheData",1,inplace=True)
+myReturnArray = joinData.to_records()
 arcpy.da.ExtendTable(fc, "FID", myReturnArray, "index", False)
 arcpy.DeleteField_management(fc, "index")
-#An index field is temporarily added, as the to_records() function appends the entire
-#  array to the table, and the array we have produced contains an index field and
-#  the new field with the natural breaks data.
-#The new field, called "Jenks", is added to the table in the specified fc parameter
-#  from before as a long integer (as the field will contain numbers representing Jenks
-#  categories, and as such will always be integers.
-#The breaks array calculated before is then transferred into this new "Jenks" field.
+arcpy.DeleteField_management(fc,"TheData")
+#Un champ intitulé "index" est temporairement ajouté au tableau du fichier géographique,
+#   et aussi un champ plus permanent intitulé "Jenks", en utilisant les fonctions arcpy.
+#Un cadre de données pandas est créé, et les resultats de l'analyse sont dirigés version
+#   le cadre.  Tous les valeurs "null" qu'on a trouvé avant sont donc identifiés comme
+#   parti du premier catégorie (le catégorie 0, parce que les catégories rangent entre
+#   0-3).
+#En utilisant la fonction to_records() de pandas, les données sont inscrites dans un
+#   tableau, est les valeurs de ce tableau sont après transferés dans les champs "index"
+#   et "Jenks".
